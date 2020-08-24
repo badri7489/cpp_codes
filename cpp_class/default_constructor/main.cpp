e04e32b4f312e3d79b018ecf581c172a8fff84e3
badri7489 <59701113+badri7489@users.noreply.github.com>
2020-08-24 20:41:41 +0530

    Constructor Initialisation

--- cpp_class/default_constructor/main.cpp
+++ cpp_class/default_constructor/main.cpp
@@ -3,25 +3,31 @@
 
 using namespace std;
 
-class Player{
+class Player
+{
 private:
 	string name;
 	int health;
 	int xp;
+
 public:
-	void set_name(string name_val){
+	void set_name(string name_val)
+	{
 		name = name_val;
 	}
-	string get_name(){
+	string get_name()
+	{
 		return name;
 	}
-	Player(){
-		name = "None";
+	Player()
+	{
+		name = "None"; // It is not initialisation it is assignment.
 		health = 100;
 		xp = 3;
 	}
 
-	Player(string name_val, int health_val, int val){
+	Player(string name_val, int health_val, int val)
+	{
 		name = name_val;
 		health = health_val;
 		xp = val;
